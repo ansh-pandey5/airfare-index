017c0eb869483fe2d1f764e86c00e98c51455aa1
ansh-pandey5 <anshpandey0508@gmail.com>
2026-09-05 13:13:28 +0530

    Use beginner-friendly APIx dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import FriendlyDashboard from './FriendlyDashboard.jsx';
 import './styles.css';
+import './friendly.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FriendlyDashboard />
   </React.StrictMode>,
 );
