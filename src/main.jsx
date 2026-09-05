291da5438c3f55de465770fefb40c7f71fea7651
ansh-pandey5 <anshpandey0508@gmail.com>
2026-09-05 13:29:19 +0530

    Fix dashboard styling conflict by removing legacy dark stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import FriendlyDashboard from './FriendlyDashboard.jsx';
-import './styles.css';
 import './friendly.css';
 
 createRoot(document.getElementById('root')).render(
